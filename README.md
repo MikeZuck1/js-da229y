214259af2c2bfc5bc0ef88a06d6a56e9fa7b4ff9
MikeZuck1 <105445747+MikeZuck1@users.noreply.github.com>
2023-10-30 08:35:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-da229y
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/MikeZuck1/js-da229y)
---
+++ index.js
@@ -0,0 +1,5 @@
+// Import stylesheets
+import './style.css';
+
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
